34350ae42195d945486887d42080f6d38bc621de
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-06 08:50:37 +0900

    Getting Location

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { Alert } from 'react-native';
 import Loader from './Loader';
+import * as Location from 'expo-location';
 
-export default function App() {
-	return <Loader />;
+export default class extends React.Component {
+	state = {
+		isLoading: true
+	};
+	getLocation = async () => {
+		try {
+			await Location.requestPermissionsAsync();
+			const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+			this.setState({ isLoading: false });
+		} catch (err) {
+			Alert.alert("I can't get location..");
+		}
+	};
+	componentDidMount() {
+		this.getLocation();
+	}
+	render() {
+		const { isLoading } = this.state;
+		return isLoading ? <Loader /> : null;
+	}
 }
