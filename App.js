9cb678df4096e8a0d379d25b2f8a98d457256dfa
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-06 06:03:00 +0900

    Layouts with FLexbox in React Native

--- App.js
+++ App.js
@@ -2,18 +2,36 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View style={styles.blueView}>
+				<Text>It's Blue</Text>
+			</View>
+			<View style={styles.yellowView}>
+				<Text>It's Yellow</Text>
+			</View>
+			<View style={styles.whiteView}>
+				<Text>It's White</Text>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'gray'
+	},
+	blueView: {
+		flex: 5,
+		backgroundColor: 'blue'
+	},
+	yellowView: {
+		backgroundColor: 'yellow',
+		flex: 5
+	},
+	whiteView: {
+		backgroundColor: 'white',
+		flex: 5
+	}
 });
